e4d536b4438b80a787f67f827b3fcc0adc68b2ac
sol <osunpine@gmail.com>
2023-05-09 00:05:37 +0900

    feat. device에 따른 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RouterProvider } from 'react-router-dom';
+import GlobalStyle from 'shared/styles/globalStyle';
+import Device from 'shared/utils/Device';
 import queryClient from 'shared/utils/queryClient';
 import Router from 'shared/utils/Router';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <RouterProvider router={Router} />
-      </div>
+      <Device>
+        <div className="App">
+          <GlobalStyle />
+          <RouterProvider router={Router} />
+        </div>
+      </Device>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
---
+++ src/shared/utils/Device.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import isMobile from './deviceDetector';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Device({ children }: Props) {
+  return (
+    <>
+      {isMobile ? (
+        <Mobile>{children}</Mobile>
+      ) : (
+        <Web>
+          <Phone>
+            <WebViewLayout>{children}</WebViewLayout>
+          </Phone>
+        </Web>
+      )}
+    </>
+  );
+}
+
+const Mobile = styled.div`
+  display: flex;
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 640px;
+  min-width: 280px;
+  overflow: hidden;
+`;
+
+const Web = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Phone = styled.div`
+  width: 426px;
+  height: 92%;
+  min-height: 750px;
+  position: fixed;
+`;
+
+const WebViewLayout = styled.div`
+  max-width: 375px;
+  height: calc(100% - 43px);
+  border-radius: 40px;
+  overflow: hidden;
+  background: blue;
+`;
