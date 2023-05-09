ec2750924b51d6f042f70a98c9dd339545ad7399
sol <osunpine@gmail.com>
2023-05-09 16:17:45 +0900

    feat. 취소 버튼 라우팅 처리 #9 & #12

--- src/pages/SignPage.tsx
+++ src/pages/SignPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 //TODO: 일반 회원 미입력시 버튼 비활성
-//TODO: 일반 회원 취소 버튼 라우팅
 
 //TODO: 관리자 회원 미입력시 버튼 비활성
-//TODO: 관리자 회원 취소 버튼 라우팅
 
 export default function SignPage() {
   const { type } = useParams();
+  const navigate = useNavigate();
+
+  const handleCancel = () => navigate('/sign');
 
   return (
     <div>
@@ -26,7 +27,7 @@ export default function SignPage() {
         </>
       )}
       <button>등록하기</button>
-      <button>취소</button>
+      <button onClick={handleCancel}>취소</button>
     </div>
   );
 }
