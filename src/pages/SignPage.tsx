440f05642bff0025f01b629e0a55fb37a7767d29
sol <osunpine@gmail.com>
2023-05-09 18:44:59 +0900

    feat. 유효성 검사 추가, 버튼 비활성화 구현 #7 & #8 & #10 & #11

--- src/pages/SignPage.tsx
+++ src/pages/SignPage.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { NAME_REGEX } from 'shared/constants/constants';
 
-//TODO: 일반 회원 미입력시 버튼 비활성
-
-//TODO: 관리자 회원 미입력시 버튼 비활성
+const validateName = (name: string): boolean => NAME_REGEX.test(name);
 
 export default function SignPage() {
   const { type } = useParams();
   const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [team, setTeam] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const handleCancel = () => navigate('/sign');
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let spacesRemovedName = e.target.value.replace(/\s/g, '');
+    setName(spacesRemovedName);
+  };
+  const handleTeam = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let spacesRemovedTeam = e.target.value.replace(/\s/g, '');
+    setTeam(spacesRemovedTeam);
+  };
+
+  useEffect(() => {
+    setIsValid(validateName(name));
+  }, [name]);
 
   return (
     <div>
       {type === 'general' ? (
         <>
-          <p>일반 회원 등록하기</p>
-          <input type="text" placeholder="이름" />
-          <input type="text" placeholder="채널코드" />
+          <p>일반 회원 등록</p>
+          <input type="text" placeholder="이름" onChange={handleName} />
+          {!!name.length && !isValid && <span>에러</span>}
+          <input type="text" placeholder="채널코드" onChange={handleTeam} />
         </>
       ) : (
         <>
-          <p>관리자 회원 등록하기</p>
-          <input type="text" placeholder="이름" />
-          <input type="text" placeholder="소속" />
+          <p>관리자 회원 등록</p>
+          <input type="text" placeholder="이름" onChange={handleName} />
+          {!!name.length && !isValid && <span>에러</span>}
+          <input type="text" placeholder="소속" onChange={handleTeam} />
         </>
       )}
-      <button>등록하기</button>
+      <button disabled={!name.length || !team.length}>등록</button>
       <button onClick={handleCancel}>취소</button>
     </div>
   );
--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,1 +1,3 @@
 export const KAKAO_OAUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+
+export const NAME_REGEX = /^[가-힣]{2,20}$/;
