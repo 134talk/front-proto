6f79cdde6c0296de0d31c32d43365407a15773b2
sol <osunpine@gmail.com>
2023-05-18 01:10:30 +0900

    feat. 아이콘 경로 상수 처리

---
+++ src/shared/constants/icons.ts
@@ -0,0 +1,12 @@
+export const LOGO_COLOR =
+  process.env.PUBLIC_URL + '/assets/logos/logo-color.svg';
+
+export const ADMIN_ICON = process.env.PUBLIC_URL + '/assets/icons/admin.svg';
+
+export const ADMIN_ACTIVE_ICON =
+  process.env.PUBLIC_URL + '/assets/icons/admin-active.svg';
+
+export const USER_ICON = process.env.PUBLIC_URL + '/assets/icons/member.svg';
+
+export const USER_ACTIVE_ICON =
+  process.env.PUBLIC_URL + '/assets/icons/member-active.svg';
