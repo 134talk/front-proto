98d9a59400fdf0d13cf0e0b1aa72b81d25f2dac6
sol <osunpine@gmail.com>
2023-05-18 10:39:46 +0900

    feat. 공통 button 수정 #83
    - 버튼 비활성화 스타일링 추가
    - border, background, color 기본값 수정

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -8,6 +8,7 @@ export interface BtnProps {
   fontWeight?: string;
   border?: string;
   bgColor?: string;
+  category: 'confirm' | 'cancel';
   text?: string;
   disabled?: boolean;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -21,11 +22,20 @@ const Btn = styled.button<BtnProps>`
   width: ${props => (props.width ? props.width : '100%')};
   margin: ${props => (props.margin ? props.margin : '0')};
   padding: ${props => (props.padding ? props.padding : '12px 32px')};
-  font-size: ${props => (props.fontSize ? props.fontSize : '16px')};
+  font-size: ${props => (props.fontSize ? props.fontSize : '1rem')};
   font-weight: ${props => (props.fontWeight ? props.fontWeight : '500')};
-  border: ${props => (props.border ? props.border : '1px solid transparent')};
-  background-color: ${props => (props.bgColor ? props.bgColor : '#FFD74B')};
-  color: black;
+  border: ${props =>
+    props.category === 'confirm'
+      ? '1px solid transparent'
+      : '1px solid #C9C8C4'};
+  background-color: ${props =>
+    props.category === 'confirm' ? '#FFD74B' : '#FFFFFF'};
+  color: #000000;
   border-radius: 50px;
-  cursor: pointer;
+  &:disabled {
+    border: 1px solid #c9c8c4;
+    background-color: #ffffff;
+    color: #c9c8c4;
+    cursor: auto;
+  }
 `;
