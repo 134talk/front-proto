b0686ee53ba6d26664b459ceb04d702ac6c4aaff
sol <osunpine@gmail.com>
2023-05-18 15:22:27 +0900

    feat. 로그인 페이지 개발 #3

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
 import { KAKAO_OAUTH_URL } from 'shared/constants/constants';
+import { LOGO } from 'shared/constants/icons';
+import { KakaoButton } from 'ui';
+import * as t from './loginPage.style';
 
 export default function LogInPage() {
   const handleLogin = () => (window.location.href = KAKAO_OAUTH_URL);
 
   return (
-    <div>
-      <button onClick={handleLogin}>카카오 로그인</button>
-    </div>
+    <>
+      <t.Container>
+        <div className="background" />
+        <img src={LOGO} alt={'로고'} />
+        <KakaoButton onClick={handleLogin} />
+      </t.Container>
+    </>
   );
 }
