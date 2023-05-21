e604c66591b3cb84cac1eac2f1403be8ca4b8dce
sol <osunpine@gmail.com>
2023-05-22 03:21:48 +0900

    feat. 공통 하단 버튼 레이아웃 개발 #138

---
+++ src/components/common/bottomButtonTab.style.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 6.75rem;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background-color: #ffffff;
+  box-shadow: 0 -2px 6px 0 rgba(172, 173, 177, 0.1);
+  padding: 0.75rem 1.25rem;
+  text-align: center;
+  color: white;
+  z-index: 100;
+`;
