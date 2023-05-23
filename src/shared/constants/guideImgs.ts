831628120dff66150e46273807b194ed9d61227d
sol <osunpine@gmail.com>
2023-05-24 03:01:33 +0900

    chore. 이미지 파일 경로 상수 처리

---
+++ src/shared/constants/guideImgs.ts
@@ -0,0 +1,7 @@
+export const GUIDE01 = process.env.PUBLIC_URL + '/assets/guide/guide01.svg';
+export const GUIDE02 = process.env.PUBLIC_URL + '/assets/guide/guide02.svg';
+export const GUIDE03 = process.env.PUBLIC_URL + '/assets/guide/guide03.svg';
+export const GUIDE04 = process.env.PUBLIC_URL + '/assets/guide/guide04.svg';
+export const GUIDE05 = process.env.PUBLIC_URL + '/assets/guide/guide05.svg';
+export const GUIDE06 = process.env.PUBLIC_URL + '/assets/guide/guide06.svg';
+export const GUIDE07 = process.env.PUBLIC_URL + '/assets/guide/guide07.svg';
