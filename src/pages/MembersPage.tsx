a630dc3ab5c558ff9ac2c80066f24f533a55e00b
sol <osunpine@gmail.com>
2023-06-08 00:47:10 +0900

    feat. 유저 본인이 목록 최상단에 위치하도록 변경

--- src/pages/MembersPage.tsx
+++ src/pages/MembersPage.tsx
@@ -1,17 +1,29 @@
 import { InviteModal, NavBar, Profile, SearchBar } from 'components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useSearchKeyword from 'shared/hooks/useSearchKeyword';
 import useTeam from 'shared/query/useTeam';
 import * as t from './membersPage.style';
 
 export default function MembersPage() {
   const [isModalOpen, setModalIsOpen] = useState(false);
+  const [myProfile, setMyProfile] = useState({});
+  const [teamListExceptMe, setTeamListExceptMe] = useState([]);
+
   const { teamList } = useTeam();
 
-  const { keyword, onDelete, handleSearch, filteredUserList } =
-    useSearchKeyword(teamList);
-
+  const uid = localStorage.getItem('uid');
   const handleModal = () => setModalIsOpen(prev => !prev);
+
+  useEffect(() => {
+    setMyProfile(teamList?.find(({ userId }) => String(userId) === uid));
+    setTeamListExceptMe(
+      teamList?.filter(({ userId }) => String(userId) !== uid) || []
+    );
+  }, [teamList, uid]);
+
+  const members = [myProfile, ...teamListExceptMe];
+  const { keyword, onDelete, handleSearch, filteredUserList } =
+    useSearchKeyword(members);
 
   return (
     <t.Container>
@@ -29,20 +41,18 @@ export default function MembersPage() {
         onDelete={onDelete}
       />
       <section>
-        {filteredUserList?.length ? (
-          <>
-            {filteredUserList.map(({ userId, nickname, name, profileUrl }) => (
-              <div key={userId}>
-                <Profile
-                  userId={userId}
-                  nickname={nickname}
-                  name={name}
-                  scale="medium"
-                  image={profileUrl}
-                />
-              </div>
-            ))}
-          </>
+        {filteredUserList?.length > 0 ? (
+          filteredUserList.map(({ userId, nickname, name, profileUrl }) => (
+            <div key={userId}>
+              <Profile
+                userId={userId}
+                nickname={nickname}
+                name={name}
+                scale="medium"
+                image={profileUrl}
+              />
+            </div>
+          ))
         ) : (
           <p className="notFound">검색 결과가 없습니다.</p>
         )}
