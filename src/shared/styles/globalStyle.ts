fe327c2f51842adaaba5383b61bb8ff1ab9289d1
sol <osunpine@gmail.com>
2023-06-09 03:11:56 +0900

    feat. theme 개발

--- src/shared/styles/globalStyle.ts
+++ src/shared/styles/globalStyle.ts
@@ -145,6 +145,13 @@ button{
 	border:none;
 	cursor: pointer;
 }
+
+@mixin flex($direction: 'row', $align: 'center', $justify: 'center') {
+    display: flex;
+    flex-direction: $direction;
+    align-items: $align;
+    justify-content: $justify;
+  }
 `;
 
 export default GlobalStyle;
