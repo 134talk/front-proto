e825d9a3a38cd9668ac215c54ec5865bdbd90840
sol <osunpine@gmail.com>
2023-06-15 00:43:26 +0900

    feat. 탭 스타일 수정

--- src/components/user/userProfile.style.ts
+++ src/components/user/userProfile.style.ts
@@ -8,19 +8,22 @@ export const Container = styled.div`
   position: absolute;
   background: ${({ theme }) => theme.white};
   z-index: 999;
-  > button {
-    width: 5.5rem;
-    height: 1rem;
-    background: none;
-    color: ${({ theme }) => theme.gray500};
-    font-size: ${({ theme }) => theme.fs14};
+  .buttonWrapper {
+    width: 100%;
     display: flex;
-    align-items: center;
-    gap: 0.25rem;
-    margin-left: calc(100% - 5rem);
-    img {
-      width: 1rem;
+    justify-content: flex-end;
+    > button {
       height: 1rem;
+      background: none;
+      color: ${({ theme }) => theme.gray500};
+      font-size: ${({ theme }) => theme.fs14};
+      display: flex;
+      align-items: center;
+      gap: 0.25rem;
+      img {
+        width: 1rem;
+        height: 1rem;
+      }
     }
   }
 `;
