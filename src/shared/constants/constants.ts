813484d0da40efc065041c657526e4bd37514df4
damiiya <ddam2lee@gmail.com>
2023-07-06 22:54:22 +0900

    fix. 상수 오타 수정

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -86,10 +86,10 @@ export const KEYWORD_LIST = [
   { id: 3, color: ['#FFD9C0', '#F8A07B', '#F18455'], keyword: '나' },
   { id: 4, color: ['#7F8EC5', '#6568A5', '#43467C'], keyword: '휴식' },
   { id: 5, color: ['#F0EEBE', '#ABCF7B', '#9BC168'], keyword: '미래/성장' },
-  { id: 9, color: ['#DCFFCB', '#69D2DE', '#02C7DE'], keyword: '여행' },
+  { id: 6, color: ['#DCFFCB', '#69D2DE', '#02C7DE'], keyword: '여행' },
   { id: 7, color: ['#C1DCFF', '#78A8F5', '#457ACF'], keyword: '팀' },
   { id: 8, color: ['#F5F2AC', '#4888A5', '#266C8B'], keyword: '커리어' },
-  { id: 6, color: ['#FFC6D6', '#FF8FA4', '#F2647D'], keyword: '사랑' },
+  { id: 9, color: ['#FFC6D6', '#FF8FA4', '#F2647D'], keyword: '사랑' },
   { id: 10, color: ['#FFBEDB', '#DDA2A7', '#CC737A'], keyword: '일' },
 ];
 
