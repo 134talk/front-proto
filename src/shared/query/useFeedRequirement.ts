815cea4e77f7702fff2bda5a4ca738df9386e17a
damiiya <ddam2lee@gmail.com>
2023-07-07 01:22:58 +0900

    fix. 대화 후 캐싱된 소켓 데이터 버그 수정

--- src/shared/query/useFeedRequirement.ts
+++ src/shared/query/useFeedRequirement.ts
@@ -1,7 +1,6 @@
 import type { AxiosError, AxiosResponse } from 'axios';
 import { useMemo } from 'react';
 import { useMutation, useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 import { getFeedRequirement, postFeedRequirement } from 'shared/api/reportApi';
 import queryKeys from 'shared/constants/queryKeys';
 
@@ -23,7 +22,6 @@ type Req = {
 };
 
 export default function useFeedRequirement() {
-  const navigate = useNavigate();
   const { data } = useQuery<AxiosResponse<Res>, AxiosError>(
     [queryKeys.FEED_REQUIREMENT],
     () => getFeedRequirement(),
@@ -60,7 +58,7 @@ export default function useFeedRequirement() {
       ),
     {
       onSuccess: () => {
-        navigate('/chats');
+        window.location.href = '/chats';
       },
     }
   );
